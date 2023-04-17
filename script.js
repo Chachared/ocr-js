78e1b80b460c5984df3ad7d0992b86a0c2b033f4
Charlotte Redier <62300614+Chachared@users.noreply.github.com>
2023-04-17 15:47:34 +0200

    (feat)write a .txt file with extracted data
    (feat)write a .txt file with extracted data

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const img = document.querySelector('img');
 const progress = document.querySelector('.progress');
 const textarea = document.querySelector('textarea');
 
+
 // afficher l'image uploadée
 fileSelector.onchange = () => {
     var file = fileSelector.files[0];
-    var imgUrl = window.URL.createObjectURL(new Blob([file], { type: 'image/jpg'}));
+    var imgUrl = window.URL.createObjectURL(new Blob([file], { type: 'image/jpg' }));
     img.src = imgUrl
 }
-
 
 //reconnaissance du texte
 start.onclick = () => {
@@ -18,15 +18,42 @@ start.onclick = () => {
     const rec = new Tesseract.TesseractWorker();
     rec.recognize(fileSelector.files[0])
         .progress(function (response) {
-            if(response.status == 'recognizing text'){
+            if (response.status == 'recognizing text') {
                 progress.innerHTML = response.status + '   ' + response.progress
-            }else{
+            } else {
                 progress.innerHTML = response.status
             }
         })
         .then(function (data) {
-            textarea.innerHTML = data.text
-            progress.innerHTML = "Done"
+            textarea.innerHTML = data.text;
+            progress.innerHTML = "Done";
+            var convertedText = data.text;
+            //première ligne du fichier
+            var txt = 'Données récupérées:\n';
+
+            //ajoute la date récupérée à la suite du fichier
+            txt += convertedText;
+            txt += "\n";
+            
+            document.write(txt);
+
+            var hiddenElement = document.createElement('a');
+            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(txt);
+            hiddenElement.target = '_blank';
+
+            //chaoix du nom du fichier créé
+            hiddenElement.download = 'test-ocr.txt';
+            hiddenElement.click();
         })
+
 }
+
+
+///faire un fichier csv et parser les données pour automatisation de la prod du csv
+///ameliorer l'affichage en evitant l'affichage des données sur une nouvelle page
+
+
+
+
+
 
