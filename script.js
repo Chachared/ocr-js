20d9cdccd6813eb8edeefbb64393ac85e7d778cb
Charlotte Redier <62300614+Chachared@users.noreply.github.com>
2023-04-18 16:37:45 +0200

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const fileSelector = document.querySelector('input');
+const fileSelector = document.querySelector('input[name="avatar"]');
+const brandSelector = document.querySelector('input[name="brand"]');
+const nameSelector = document.querySelector('input[name="name"]');
 const start = document.querySelector('button');
 const img = document.querySelector('img');
 const progress = document.querySelector('.progress');
@@ -26,30 +28,45 @@ start.onclick = () => {
         })
         .then(function (data) {
             textarea.innerHTML = data.text;
-            progress.innerHTML = "Done";
+            progress.innerHTML = "OCR de l'image terminée";
             var convertedText = data.text;
+            const lines = convertedText.split('\n');
+
             //première ligne du fichier
-            var txt = 'Données récupérées:\n';
+            var csv = 'marque, modèle, années de production, converti par\n';
+
+            //récupérer la marque et le nom saisis par l'utilisateur
+            var brand = brandSelector.value;
+            var addedBy = nameSelector.value;
 
             //ajoute la data récupérée à la suite du fichier
-            txt += convertedText;
-            txt += "\n";
-            
+            for (const line of lines) {
+                const words = line.split(' ')
+                const years = words.splice(-1, 1)
+                const model = words.join(' ')
+                console.log(model, years)
+                csv += brand;
+                csv += ",";
+                csv += model;
+                csv += ",";
+                csv += years;
+                csv += ",";
+                csv += addedBy;
+                csv += "\n";
+            }
+
 
             var hiddenElement = document.createElement('a');
-            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(txt);
+            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
             hiddenElement.target = '_blank';
 
-            //chaoix du nom du fichier créé
-            hiddenElement.download = 'test-ocr.txt';
+            //choix du nom du fichier créé
+            hiddenElement.download = 'test-ocr.csv';
             hiddenElement.click();
         })
-
 }
 
-
-///faire un fichier csv et parser les données pour automatisation de la prod du csv
-///ameliorer l'affichage en evitant l'affichage des données sur une nouvelle page
+///produire un csv unique (rajouter un if csv is true -> add, else -> create)
 
 
 
